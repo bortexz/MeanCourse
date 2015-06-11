0bee0efacad18362c51e76b8088ddc51f8652154
Alberto Fernandez <alberto.fernandez.491@gmail.com>
2015-06-11 16:55:22 +0200

    Finished example 11.

--- example_11_angular_forms/app.js
+++ example_11_angular_forms/app.js
@@ -5,4 +5,9 @@ function FormController($scope) {
     showDueDate: false
   };
   $scope.today = new Date();
+
+  $scope.createTask = function() {
+    $scope.task = "Created task " + $scope.formData.title;
+    if($scope.formData.showDueDate && $scope.formData.dueDate ) $scope.task += " to do before " + $scope.formData.dueDate.toString();
+  }
 }
