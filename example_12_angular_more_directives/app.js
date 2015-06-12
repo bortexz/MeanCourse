e2d2c17eef78a394b60eafa976121b4852cd4ac7
Alberto Fernandez <alberto.fernandez.491@gmail.com>
2015-06-12 12:34:51 +0200

    Finished example 12

--- example_12_angular_more_directives/app.js
+++ example_12_angular_more_directives/app.js
@@ -1,4 +1,22 @@
 /**
  * Created by alberto on 9/6/15.
  */
-//angular.module('directives',[]);
+angular.module('directives',['ngMessages']).controller('TasksController', TasksController);
+
+function TasksController($scope) {
+  $scope.formData = {
+    showDueDate: false
+  };
+
+  $scope.today = new Date();
+
+  $scope.createTask = function() {
+    $scope.tasks.push({
+      title: $scope.formData.title,
+      description: $scope.formData.description,
+      dueDate: $scope.formData.dueDate ? $scope.formData.dueDate : null
+    });
+  };
+
+  $scope.tasks = [];
+}
