d7b0dd8a2a476aadfa68d2e6167ee06cf59357e6
Alberto Fernandez <alberto.fernandez.491@gmail.com>
2015-07-10 14:19:28 +0200

    grunt front and back

---
+++ Examples/example_22_front_and_back/Gruntfile.js
@@ -0,0 +1,44 @@
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-concurrent');
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      options: {
+        livereload:true
+      },
+      scripts: {
+        files: ['**/*.js'],
+        tasks: []
+      }
+    },
+    connect: {
+      task: { // give your task a name
+        options: {
+          port: 3000, // configure your port here
+          base: 'public/',
+          keepalive: true,
+          livereload: true
+        }
+      }
+    },
+    nodemon: {
+      dev: {
+        script: 'server.js'
+      }
+    },
+    concurrent: {
+      options: {
+        logConcurrentOutput: true
+      },
+      tasks: ['nodemon','connect', 'watch' ]
+    }
+  });
+
+  // task setup
+  grunt.registerTask('default', ['concurrent']);
+};
---
+++ Examples/example_22_front_and_back/public/services/tasks.js
@@ -0,0 +1,47 @@
+angular.module('services').factory('tasksService', tasksService);
+
+function tasksService($http, $q, AuthService) {
+  var SERVER_URL = 'http://localhost:8080/';
+
+  function getTasks() {
+    var q = $q.defer();
+
+    if(AuthService.user) {
+      console.log(AuthService.getUser());
+      $http.get(SERVER_URL + 'user/' + AuthService.getUser().user._id + '/task').then(function(data){
+        q.resolve(data);
+      }, function(){
+        q.reject();
+      });
+    } else {
+      q.reject();
+    }
+    return q.promise;
+  }
+
+  function addTask(newTask) {
+    var q = $q.defer();
+    $http.post(SERVER_URL, newTask).then(function(data){
+      q.resolve(data)
+    }, function(){
+      q.reject(data);
+    });
+    return q.promise;
+  }
+
+  function removeTask(id) {
+    var q = $q.defer();
+    $http.delete(SERVER_URL + id).then(function(data){
+      q.resolve(data);
+    }, function(data) {
+      q.reject(data);
+    });
+    return q.promise;
+  }
+
+  return {
+    getTasks: getTasks,
+    addTask: addTask,
+    removeTask: removeTask
+  }
+}
